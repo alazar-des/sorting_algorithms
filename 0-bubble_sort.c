5df5bf695bd584f1f0aa02531cf10b563def26ea
alazar-des <alazar.des@gmail.com>
2021-06-16 18:29:47 +0300

    bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * swap - swap to integers
+ * @a: first integer input
+ * @b: second integer input
+ */
+void swap(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
+ * bubble_sort - bubble sort algorithm
+ * @array: array to be sorted
+ * @size: size of array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int i, j;
+
+	for (i = size; i > 0; i--)
+	{
+		for (j = 1; j < i; j++)
+		{
+			if (array[j] < array[j - 1])
+			{
+				swap(&array[j], &array[j - 1]);
+				print_array(array, size);
+			}
+		}
+	}
+}
