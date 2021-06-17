bd4561b8ab2f91437eae2c57a40deb06e508a821
murageB <brendanrichard96@gmail.com>
2021-06-17 21:45:44 +0000

    shell sort

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -9,32 +9,32 @@
 
 void shell_sort(int *array, size_t size)
 {
-  int inner, outer, valueToInsert;
-  int interval = 1;
-  int elements = (int)size;
+int inner, outer, valueToInsert;
+int interval = 1;
+int elements = (int)size;
 
 if (!array || !size || size < 2)
-  return;
+return;
 
- while (interval <= elements/3)
-   {
-     interval = interval * 3 + 1;
-   }
- while (interval > 0)
-   {
-     for (outer = interval; outer < elements; outer++)
-       {
-	 valueToInsert = array[outer];
-	 inner = outer;
+while (interval <= elements / 3)
+{
+interval = interval * 3 + 1;
+}
+while (interval > 0)
+{
+for (outer = interval; outer < elements; outer++)
+{
+valueToInsert = array[outer];
+inner = outer;
 
-	 while (inner > interval - 1 && array[inner - interval] >= valueToInsert)
-	   {
-	     array[inner] = array[inner - interval];
-	     inner -= interval;
-	   }
-	 array[inner] = valueToInsert;
-       }
-     interval = interval / 3;
-     print_array(array, size);
-   }
+while (inner > interval - 1 && array[inner - interval] >= valueToInsert)
+{
+array[inner] = array[inner - interval];
+inner -= interval;
+}
+array[inner] = valueToInsert;
+}
+interval = interval / 3;
+print_array(array, size);
+}
 }
