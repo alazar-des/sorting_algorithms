a5a23ca4f76984d6b57574fdebd9fda53e1d9b01
murageB <brendanrichard96@gmail.com>
2021-06-17 21:15:49 +0000

    shell sort

---
+++ 100-shell_sort.c
@@ -0,0 +1,37 @@
+#include "sort.h"
+
+/**
+ * shell_sort - sorts an array using the Shell sort algorithm
+ * @array: array to be sorted
+ * @size: size of the array
+ * Return: void
+ */
+
+void shell_sort(int *array, size_t size)
+{
+int inner, outer, valueToInsert;
+int interval = 1;
+int elements = (int)size;
+
+while (interval <= elements/3)
+{
+interval = interval * 3 + 1;
+}
+while (interval > 0)
+{
+for (outer = interval; outer < elements; outer++)
+{
+valueToInsert = array[outer];
+inner = outer;
+
+while (inner > interval - 1 && array[inner - interval] >= valueToInsert)
+{
+array[inner] = array[inner - interval];
+inner -= interval;
+}
+array[inner] = valueToInsert;
+}
+interval = interval / 3;
+print_array(array, size);
+}
+}
